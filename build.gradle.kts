d5c706a83a914fc5b461e3be47985b099341f2ab
burnoo <bruno.wieczorek@gmail.com>
2021-11-21 20:44:00 +0100

    Add dependencyUpdates plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,10 @@
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
+
+@Suppress("DSL_SCOPE_VIOLATION")
+plugins {
+    alias(libs.plugins.dependencyUpdates)
+}
+
 buildscript {
     repositories {
         google()
@@ -13,5 +20,21 @@ allprojects {
     repositories {
         google()
         mavenCentral()
+    }
+}
+
+tasks.withType<DependencyUpdatesTask> {
+    rejectVersionIf {
+        getStabilityLevel(candidate.version) < getStabilityLevel(currentVersion)
+    }
+}
+
+fun getStabilityLevel(version: String) : Int {
+    val v = version.toUpperCase()
+    return when {
+        v.contains("ALPHA") -> 0
+        v.contains("BETA") -> 1
+        v.contains(Regex("(?:RC)|(?:-M[0-9]+)")) -> 2
+        else -> 3
     }
 }
